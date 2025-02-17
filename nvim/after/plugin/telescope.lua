a69905952642d4b733f13216fd0f1b722384f320
Matteo Bucci <you@example.com>
2025-02-17 14:10:44 +0100

    added better mappings to live_grep (nvim telescope)

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,32 @@
+local actions = require('telescope.actions')
+
+require('telescope').setup{
+	defaults = {
+		mappings = {
+			i = {
+				['<C-j>'] = actions.move_selection_next, -- move to next result
+				['<C-k>'] = actions.move_selection_previous, -- move to prev result
+				['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+				['<esc>'] = actions.close,
+				['<CR>'] = actions.select_default + actions.center,
+			},
+			n = {
+				['<C-j>'] = actions.move_selection_next, -- move to next result
+				['<C-k>'] = actions.move_selection_previous, -- move to prev result
+				['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+				['<esc>'] = actions.close,
+				['<CR>'] = actions.select_default + actions.center,
+			},
+		}
+	}
+}
+
+
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>gf', builtin.live_grep, {})
---vim.keymap.set('n', '<leader>ps', function()
---	builtin.grep_string({ search = vim.fn.input("Grep > ") })
---end)
+vim.keymap.set('n', '<leader>gr', function()
+	builtin.grep_string({ search = vim.fn.input('Grep > ') })
+end)
